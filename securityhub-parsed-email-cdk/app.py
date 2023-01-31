8995f89f6e9b5116f4860ba0d323c58c5e92191c
shephexd <shephexd@gmail.com>
2023-01-31 15:09:27 +0900

    UPDATE VPC SetupStack

--- securityhub-parsed-email-cdk/app.py
+++ securityhub-parsed-email-cdk/app.py
@@ -3,9 +3,11 @@
 import aws_cdk as cdk
 
 from stacks.security_notification import SecurityEventNotificationStack
+from stacks.vpc_setup_stack import VPCSetupStack
 
 
 app = cdk.App()
-SecurityEventNotificationStack(app, "SecurityEventNotificationStack")
+# SecurityEventNotificationStack(app, "SecurityEventNotificationStack")
+VPCSetupStack(app, "DemoVPC")
 
 app.synth()
